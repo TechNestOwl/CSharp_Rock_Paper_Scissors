fb07e953d4b919ca4c8b7d7a7835479a21c510bb
Guilherme Guimaraes <technestowl@gmail.com>
2024-09-25 11:54:03 -0400

    Added complete logic for rock, paper, scissors

--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,7 @@
 
 using System.Security.Cryptography;
 
-// generate a random number 1 - 3 on page load. (do not display) 
-Random random = new Random();
-int randomNumber = random.Next(1, 3);
+
 
 namespace Rock_Paper_Scissors
 {
@@ -25,22 +23,66 @@ namespace Rock_Paper_Scissors
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+
         }
 
         private void shoot_button_Click(object sender, EventArgs e)
         {
+            // generate a random number 1 - 3(do not display) 
+            Random random = new Random();
+            int randomNumber = random.Next(1, 4);
+
+            //check user rock,paper,scissor selection
+            int userSelection = 0;
+
             if (rockRadio.Checked)
             {
-                int userSelection = 1;
+                //rock
+                userSelection = 1;
             }
             else if (paperRadio.Checked)
             {
-                int userSelection = 2;
+                //paper
+                userSelection = 2;
             }
             else if (scissorsRadio.Checked)
             {
-                int userSelection = 3;
+                //scissors
+                userSelection = 3;
             }
+
+            System.Diagnostics.Debug.WriteLine(randomNumber);
+            System.Diagnostics.Debug.WriteLine(userSelection);
+
+
+            if (userSelection == randomNumber)
+            {
+                MessageBox.Show("Draw!");
+            }
+            else if (userSelection == 1 && randomNumber == 2)
+            {
+                MessageBox.Show(" Rock vs Paper - You loose");
+            }
+            else if (userSelection == 2 && randomNumber == 3)
+            {
+                MessageBox.Show("Paper vs Scissors - You Loose");
+            }
+            else if (userSelection == 3 && randomNumber == 1)
+            {
+                MessageBox.Show("Scissors vs Rock - You Loose");
+            }else if (userSelection == 1 && randomNumber == 3)
+            {
+                MessageBox.Show("Rock vs Scissors - You Win!");
+            }else if(userSelection == 2 && randomNumber == 1)
+            {
+                MessageBox.Show("Paper vs Rock - You Win!");
+            }else if(userSelection == 3 && randomNumber == 2)
+            {
+                MessageBox.Show("Scissors vs Paper - You Win!");
+            }
+
+
 
         }
     }
