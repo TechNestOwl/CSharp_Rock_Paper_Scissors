cd919615b2d4d0ef7d247a460a0e139f4ff7f71c
Guilherme Guimaraes <technestowl@gmail.com>
2024-09-25 12:18:36 -0400

    Spread logic into two methods outside of button click

--- Form1.cs
+++ Form1.cs
@@ -27,6 +27,61 @@ namespace Rock_Paper_Scissors
 
         }
 
+        int readUserChoice()
+        {
+            if (rockRadio.Checked)
+            {
+                return 1; //rock
+            }
+            else if (paperRadio.Checked)
+            {
+                return 2; //paper
+            }
+            else if (scissorsRadio.Checked)
+            {
+                return 3; //scissors
+            }
+            else {
+                return 0;
+            }
+        }
+
+        void determineWinner(int user, int bot)
+        {
+            if(user == 0)
+            {
+                MessageBox.Show("Please select your weapon");
+            }
+            else if (user == bot)
+            {
+                MessageBox.Show("Draw!");
+            }
+            else if (user == 1 && bot == 2)
+            {
+                MessageBox.Show(" Rock vs Paper - You loose");
+            }
+            else if (user == 2 && bot == 3)
+            {
+                MessageBox.Show("Paper vs Scissors - You Loose");
+            }
+            else if (user == 3 && bot == 1)
+            {
+                MessageBox.Show("Scissors vs Rock - You Loose");
+            }
+            else if (user == 1 && bot == 3)
+            {
+                MessageBox.Show("Rock vs Scissors - You Win!");
+            }
+            else if (user == 2 && bot == 1)
+            {
+                MessageBox.Show("Paper vs Rock - You Win!");
+            }
+            else if (user == 3 && bot == 2)
+            {
+                MessageBox.Show("Scissors vs Paper - You Win!");
+            }
+        }
+
         private void shoot_button_Click(object sender, EventArgs e)
         {
             // generate a random number 1 - 3(do not display) 
@@ -34,56 +89,12 @@ namespace Rock_Paper_Scissors
             int randomNumber = random.Next(1, 4);
 
             //check user rock,paper,scissor selection
-            int userSelection = 0;
+            int userSelection = readUserChoice();
 
-            if (rockRadio.Checked)
-            {
-                //rock
-                userSelection = 1;
-            }
-            else if (paperRadio.Checked)
-            {
-                //paper
-                userSelection = 2;
-            }
-            else if (scissorsRadio.Checked)
-            {
-                //scissors
-                userSelection = 3;
-            }
+            determineWinner(userSelection, randomNumber);
 
             System.Diagnostics.Debug.WriteLine(randomNumber);
             System.Diagnostics.Debug.WriteLine(userSelection);
-
-
-            if (userSelection == randomNumber)
-            {
-                MessageBox.Show("Draw!");
-            }
-            else if (userSelection == 1 && randomNumber == 2)
-            {
-                MessageBox.Show(" Rock vs Paper - You loose");
-            }
-            else if (userSelection == 2 && randomNumber == 3)
-            {
-                MessageBox.Show("Paper vs Scissors - You Loose");
-            }
-            else if (userSelection == 3 && randomNumber == 1)
-            {
-                MessageBox.Show("Scissors vs Rock - You Loose");
-            }else if (userSelection == 1 && randomNumber == 3)
-            {
-                MessageBox.Show("Rock vs Scissors - You Win!");
-            }else if(userSelection == 2 && randomNumber == 1)
-            {
-                MessageBox.Show("Paper vs Rock - You Win!");
-            }else if(userSelection == 3 && randomNumber == 2)
-            {
-                MessageBox.Show("Scissors vs Paper - You Win!");
-            }
-
-
-
         }
     }
 }
